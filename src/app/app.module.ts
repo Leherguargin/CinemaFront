e1d246184bdad9788e14ae0e9306adea8c4082cf
Leherguargin <maciejleszczynski1997@gmail.com>
2023-02-27 05:33:10 +0100

    add components generated with angular material schematics

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,73 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
+import {ReactiveFormsModule} from '@angular/forms';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {HomeComponent} from './component/home/home.component';
 
+import {AddressFormComponent} from './component/schematics/address-form/address-form.component';
+import {NavigationComponent} from './component/schematics/navigation/navigation.component';
+
+import {LayoutModule} from '@angular/cdk/layout';
+
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatMenuModule} from '@angular/material/menu';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatListModule} from '@angular/material/list';
+import {MatInputModule} from '@angular/material/input';
+import {MatSelectModule} from '@angular/material/select';
+import {MatRadioModule} from '@angular/material/radio';
+import {MatCardModule} from '@angular/material/card';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { TableComponent } from './component/schematics/table/table.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { DashboardComponent } from './component/schematics/dashboard/dashboard.component';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { TreeComponent } from './component/schematics/tree/tree.component';
+import { MatTreeModule } from '@angular/material/tree';
+import { DragAndDropComponent } from './component/schematics/drag-and-drop/drag-and-drop.component';
+import { DragDropModule } from '@angular/cdk/drag-drop';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    AddressFormComponent,
+    NavigationComponent,
+    TableComponent,
+    DashboardComponent,
+    TreeComponent,
+    DragAndDropComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
+    LayoutModule,
     MatToolbarModule,
     MatIconModule,
     MatButtonModule,
     MatMenuModule,
-    BrowserAnimationsModule,
+    MatDividerModule,
+    MatListModule,
+    MatInputModule,
+    MatSelectModule,
+    MatRadioModule,
+    MatCardModule,
+    MatSidenavModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatGridListModule,
+    MatTreeModule,
+    DragDropModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
